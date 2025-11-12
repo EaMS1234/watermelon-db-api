d9dc3e30526dfe8930e1c54c7ebdeef99b66b8bb
Erick Augusto <erickaugusto1234@gmail.com>
2025-11-11 21:05:45 -0300

    Mensagens de erro e log para o crud de corpo-localização

--- crud/corpo_localizacao.go
+++ crud/corpo_localizacao.go
@@ -20,15 +20,21 @@ func (Corpo_Localizacao) TableName() string {
 
 
 func GetCorpoLocal (w http.ResponseWriter, r *http.Request) {
-	log.Output(0, "GET Local ID_Corpo_d_agua = " + r.PathValue("id"))
+	log.Output(1, r.RemoteAddr + " GET Local ID_Corpo_d_agua = " + r.PathValue("id"))
 
 	db := banco.Banco()
 
 	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {panic(err)}
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest) // não foi possível converter em inteiro
+		return
+	}
 
 	var corpo_local []Corpo_Localizacao
-	db.Find(&corpo_local, "ID_Corpo_d_agua = ?", id)
+	if db.Find(&corpo_local, "ID_Corpo_d_agua = ?", id).Error != nil {
+		http.Error(w, "ID inexistente", http.StatusNotFound)
+		return
+	}
 
 	var locais []Localizacao
 	for _, v := range corpo_local {
@@ -43,15 +49,21 @@ func GetCorpoLocal (w http.ResponseWriter, r *http.Request) {
 }
 
 func GetLocalCorpo (w http.ResponseWriter, r *http.Request) {
-	log.Output(0, "GET Corpo ID_Localizacao = " + r.PathValue("id"))
+	log.Output(1, r.RemoteAddr + " GET Corpo ID_Localizacao = " + r.PathValue("id"))
 
 	db := banco.Banco()
 
 	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {panic(err)}
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest) // não foi possível converter em inteiro
+		return
+	}
 
 	var corpo_local []Corpo_Localizacao
-	db.Find(&corpo_local, "ID_Localizacao = ?", id)
+	if db.Find(&corpo_local, "ID_Localizacao = ?", id).Error != nil {
+		http.Error(w, "ID inexistente", http.StatusNotFound)
+		return
+	}
 
 	var corpos []Corpo
 	for _, v := range corpo_local {
@@ -63,14 +75,16 @@ func GetLocalCorpo (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(corpos)
-
 }
 
 func PostCorpoLocal (w http.ResponseWriter, r *http.Request) {
-	log.Output(0, "POST Local ID_Corpo_d_agua = " + r.PathValue("id"))
+	log.Output(1, r.RemoteAddr + " POST Local ID_Corpo_d_agua = " + r.PathValue("id"))
 
 	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {panic(err)}
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest) // não foi possível converter em inteiro
+		return
+	}
 
 	var corpo_local Corpo_Localizacao
 	json.NewDecoder(r.Body).Decode(&corpo_local)
@@ -81,14 +95,23 @@ func PostCorpoLocal (w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteCorpoLocal (w http.ResponseWriter, r *http.Request) {
-	log.Output(0, "DELETE Local ID_Localizacao " + r.PathValue("id_local") + " para o ID_Corpo_d_agua = " + r.PathValue("id"))
+	log.Output(1, r.RemoteAddr + " DELETE Local ID_Localizacao " + r.PathValue("id_local") + " para o ID_Corpo_d_agua = " + r.PathValue("id"))
 
 	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {panic(err)}
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest) // não foi possível converter em inteiro
+		return
+	}
 
 	id_local, err := strconv.Atoi(r.PathValue("id_local"))
-	if err != nil {panic(err)}
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest) // não foi possível converter em inteiro
+		return
+	}
 
-	banco.Banco().Delete(&Corpo_Localizacao{}, "ID_Corpo_d_agua = ? AND ID_Localizacao = ?", id, id_local)
+	if banco.Banco().Delete(&Corpo_Localizacao{}, "ID_Corpo_d_agua = ? AND ID_Localizacao = ?", id, id_local).Error != nil {
+		http.Error(w, "ID inexistente", http.StatusNotFound)
+		return
+	}
 }
 
