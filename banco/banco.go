3396fb4d535610c13bb081a3f7b75d7bf517d038
Erick Augusto <erickaugusto1234@gmail.com>
2025-11-23 18:50:04 -0300

    Adicionado o volume persistente ao compose

--- banco/banco.go
+++ banco/banco.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Banco() *gorm.DB {
-	db, err := gorm.Open(mysql.Open("watermelon:watermelon@tcp(127.0.0.1:3306)/watermelon_db"), &gorm.Config{
+	db, err := gorm.Open(mysql.Open("watermelon:watermelon@tcp(db:3306)/watermelon_db"), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
         SingularTable: true,
 				NoLowerCase: true,
